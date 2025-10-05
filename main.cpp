9ac6b88fc94c5d8c2249ed4c27b7cb23c25be1fd
Undeciphered <arseneault3luc@gmail.com>
2025-10-05 17:14:01 -0300

    Add opencv video editing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <string>
 #include <serial/serial.h>
+#include <opencv2/opencv.hpp>
 
 int main() {
+ 
 
+
+    cv::VideoCapture cap("C:/Users/CSAP/Downloads/Bad Apple.mp4");
+    cv::Mat frame, gray, resized, binary, output;
+    
+    while (cap.read(frame)) {
+        cv::resize(frame, resized, cv::Size(23, 17));
+        cv::cvtColor(resized, gray, cv::COLOR_BGR2GRAY);
+        cv::threshold(gray, binary, 127, 255, cv::THRESH_BINARY);
+        cv::resize(binary, output, cv::Size(690, 510), cv::INTER_NEAREST);
+        cv::imshow("First Frame", output);
+        for (;;) {
+           int input = cv::waitKey(0) & 0xff;
+           if (input == 27) return 0;
+           if (input == 'q' || input == 'Q') break;
+        }
+    }
+
+    /*
     std::cout << "[ports]\n";
     auto ports = serial::list_ports();
     for (auto port : ports) {
@@ -21,5 +41,5 @@ int main() {
     std::cout << receive_buffer << '\n';
 
     arduino_serial.close();
-
+    */
 }
