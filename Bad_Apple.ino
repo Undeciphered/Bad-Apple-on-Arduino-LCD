7e97466e6ebce829fabf3d7d4397e858b2c2875b
Undeciphered <arseneault3luc@gmail.com>
2025-10-25 19:23:54 -0300

    Refactor loop to handle multiple screen segments

--- Bad_Apple.ino
+++ Bad_Apple.ino
@@ -16,13 +16,27 @@ void setup() {
   }
 }
 
-uint8_t receive_buffer
+uint8_t receive_buffer{0};
+uint8_t screen_segment_buffer[8];
 
 void loop() {
-  
-  while (Serial.available() == 0);
-  receive_buffer = Serial.read();
-  lcd.setCursor(0, 0);
-  lcd.print(receive_buffer);
-  lcd.print("     ");
+
+  for (int segment = 0; segment < 8; segment++) {
+    for (int line = 0; line < 8; line++) {
+
+      while (Serial.available() == 0);
+      receive_buffer = Serial.read();
+      screen_segment_buffer[line] = receive_buffer;
+    }
+    lcd.createChar(segment, screen_segment_buffer);
+  }
+
+  for (int row = 0; row <= 1; row++) {
+    lcd.setCursor(0, row);
+    for(int i = 0; i < 4; i++) {
+      lcd.write(byte(i + row * 4));
+    }
+  }
+
+  lcd.clear();
 }
