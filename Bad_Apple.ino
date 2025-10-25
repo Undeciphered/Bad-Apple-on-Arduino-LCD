4fb7f62cf0d6f8ec079310005ee3bf2f477ea0cc
Undeciphered <arseneault3luc@gmail.com>
2025-10-25 16:23:53 -0300

    Implement LCD display for serial input
    Added LCD display functionality to show received serial data.

--- Bad_Apple.ino
+++ Bad_Apple.ino
@@ -1,11 +1,27 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
 void setup() {
+
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.clear();
 }
 
 void loop() {
 
+  while (Serial.available() == 0);
+  int handshake_receive_buffer = Serial.read();
+  if(handshake_receive_buffer == 6) {
+    uint8_t handshake_send_buffer{7};
+    Serial.write(handshake_send_buffer);
+  } else {
+    return;
+  }
 
   while (Serial.available() == 0);
   String receive_buffer = Serial.readStringUntil('\n');
-  Serial.write("Hello World!\n");
-}
+  lcd.setCursor(0, 0);
+  lcd.print(receive_buffer);
+  Serial.println("Hello World!");
+  }
