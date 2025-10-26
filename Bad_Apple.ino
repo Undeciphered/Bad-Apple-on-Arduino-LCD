7ab99f217fcadc2733a66bc472e06a72fea72215
Undeciphered <arseneault3luc@gmail.com>
2025-10-26 11:33:17 -0300

    Improve data reception and LCD rendering
    Refactor data handling and LCD display logic for better performance.

--- Bad_Apple.ino
+++ Bad_Apple.ino
@@ -3,7 +3,7 @@
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 void reset() {
-  
+
   wdt_enable(WDTO_15MS);
   for (;;);
 }
@@ -15,40 +15,43 @@ void setup() {
   lcd.clear();
   while (Serial.available() == 0);
   int handshake_receive_buffer = Serial.read();
-  if(handshake_receive_buffer == 6) {
-    uint8_t handshake_send_buffer{7};
+  if (handshake_receive_buffer == 6) {
+    uint8_t handshake_send_buffer{ 7 };
     Serial.write(handshake_send_buffer);
   } else {
     reset();
   }
 }
 
-uint8_t receive_buffer{0};
 uint8_t screen_segment_buffer[8];
+uint8_t buffer[64];
+uint8_t byte_count{ 0 };
+unsigned long since_last_data{0};
 
 void loop() {
 
-  for (int segment = 0; segment < 8; segment++) {
-    for (int line = 0; line < 8; line++) {
+  while (Serial.available() > 0 && byte_count < 64) {
+    buffer[byte_count++] = Serial.read();
+    since_last_data = millis();
+  }
 
-      auto since_last_data = millis();
-      while (Serial.available() == 0) {
-        if (millis() - since_last_data >= 1000) {
-          lcd.clear();
-          reset();
-        }
-      };
-      receive_buffer = Serial.read();
-      screen_segment_buffer[line] = receive_buffer;
+  if (byte_count == 64) {
+
+    for (int segment = 0; segment < 8; segment++) {
+      for (int line = 0; line < 8; line++) {
+        screen_segment_buffer[line] = buffer[8 * segment + line];
+      }
+      lcd.createChar(segment, screen_segment_buffer);
     }
-    lcd.createChar(segment, screen_segment_buffer);
-  }
-  
-  lcd.clear();
-  for (int row = 0; row <= 1; row++) {
-    lcd.setCursor(0, row);
-    for(int i = 0; i < 4; i++) {
-      lcd.write(byte(i + row * 4));
+
+    for (int row = 0; row <= 1; row++) {
+      lcd.setCursor(0, row);
+      for (int i = 0; i < 4; i++) {
+        lcd.write(byte(i + row * 4));
+      }
     }
+    byte_count = 0;
   }
+
+  if (millis() - since_last_data >= 1000 && since_last_data != 0) reset();
 }
